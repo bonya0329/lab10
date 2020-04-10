a83f6f0ee35190d5707fd85d1704f744e46e97ce
bonya0329 <60532283+bonya0329@users.noreply.github.com>
2020-04-11 02:03:32 +0600

    Добавьте файлы проекта.

---
+++ lab10/Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab10.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Id(string name, string data, string group)
+        {
+
+            string[] dataSplit = data.Split('.');
+            string s = name + dataSplit[0] + dataSplit[1] + dataSplit[2] + group;
+            string value = RandomString(s);
+
+            return $"Привет! {name} + your id = {value}";
+        }
+        private static Random random = new Random();
+        public static string RandomString(string str)
+        {
+            return new string(Enumerable.Repeat(str, 8)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
